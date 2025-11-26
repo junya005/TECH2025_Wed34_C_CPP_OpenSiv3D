e35f4127af555b72d9ca49397b087fe6e6969273
junya005 <junya005.65@gmail.com>
2025-11-26 15:28:59 +0900

    20251126

---
+++ 20251126_suica_sample/Main.cpp
@@ -0,0 +1,303 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.15
+
+enum E_Fruit_Type
+{
+	Sakuranbo,
+	Ichigo,
+	Budou,
+	Dekopon,
+	Kaki,
+	Ringo,
+	Momo,
+	Pin,
+	Meron,
+	Suica,
+	MAX
+};
+
+
+class Fruit
+{
+public:
+
+	bool IsDead = false;
+
+	E_Fruit_Type Type = Sakuranbo;
+
+	Fruit(float x, float y, E_Fruit_Type type)
+	{
+		Type = type;
+		IsDead = false;
+
+		// 銀河の中心からの距離と、角度をRandomに決定する
+		_x = x;
+		_y = y;
+
+		float cross = (int)type * 1.3f;
+		if (cross != 0.0f) {
+			_radius = 5.0f *cross;
+		}
+		else
+		{
+			_radius = 5.0f;
+		}
+
+		switch (type)
+		{
+		case Sakuranbo:
+			_color = ColorF( 0.9f,0.2f,0.1f );
+			break;
+		case Ichigo:
+			_color = ColorF(0.5f, 0.15f, 0.15f);
+			break;
+		case Budou:
+			_color = ColorF(0.25f, 0.6f, 0.095f);
+			break;
+		case Dekopon:
+			_color = ColorF(0.65f, 0.1f, 0.65f);
+			break;
+		case Kaki:
+			_color = ColorF(0.6f, 0.1f, 0.05f);
+			break;
+		case Ringo:
+			_color = ColorF(0.9f, 0.05f, 0.05f);
+			break;
+		case Momo:
+			_color = ColorF(0.95f, 0.75f, 0.75f);
+			break;
+		case Pin:
+			_color = ColorF(0.95f, 0.7f, 0.15f);
+			break;
+		case Meron:
+			_color = ColorF(0.95f, 0.7f, 0.15f);
+			break;
+		case Suica:
+			_color = ColorF(0.15f, 0.45f, 0.125f);
+			break;
+		case MAX:
+			break;
+		default:
+			break;
+		}
+	}
+
+	void Logic(float deltaTime)
+	{
+		if (IsDead) return;
+
+		// 加速度反映 動作反映
+		_x += _accelX * deltaTime;
+		_y += _accelY * deltaTime;
+
+		// 壁当たり
+		if(_x > 600 - _radius)
+		{
+			_x = 600 - _radius;
+
+			if (_accelX > 0) _accelX *= -0.25f;
+		}
+
+		if (_x < 200 - _radius)
+		{
+			_x = 200 - _radius;
+
+			if (_accelX > 0) _accelX *= -0.25f;
+		}
+
+		if(_y > 500)
+		{
+			_y = 500 - _radius;
+
+			if (_accelY > 0) _accelY *= -0.25f;
+		}
+
+		// 振動禁止
+		if (_accelX >= -0.01f && _accelX <= 0.01f)_accelX = 0.0f;
+		if (_accelY >= -0.01f && _accelY <= 0.01f)_accelY = 0.0f;
+
+		// 重力
+		_accelY += deltaTime * CONST_GRAVITY_SCALE;
+
+		if(_y >= 600.0f + _radius)
+		{
+			IsDead = true;
+		}
+	}
+
+	void Draw()
+	{
+		if (IsDead) return;
+
+		Circle{ _x, _y, _radius }.draw(_color);
+	}
+
+	void SearchAndFusionAndDestroy(Fruit& another)
+	{
+
+		if (_radius < 0) return;
+		if (another._radius < 0) another.IsDead = true;
+
+		Fruit* p1 = this;
+		Fruit* p2 = &another;
+
+		if (p1 == p2)
+		{
+			return;
+		}
+
+		float distX = another._x - _x;
+		float distY = another._y - _y;
+		float sqrDistance = distX * distX + distY * distY;
+		float plusRadius = another._radius + _radius;
+
+		// 二つの文明の活動領域がかぶった
+		if (plusRadius * plusRadius >= sqrDistance)
+		{
+			float colTheta = Math::Atan2(distY,distX) + Math::Pi * 0.5f;
+			// colTheta + 360 = this + ref
+			float refTheta = colTheta + Math::Pi * 2.0f - Math::Atan2(_accelY, _accelX);
+
+			float mag = Math::Sqrt(Math::Pow(_accelY, 2) + Math::Pow(_accelX, 2));
+
+			float nomalizeX = Math::Cos(refTheta);
+			float nomalizeY = Math::Sin(refTheta);
+
+			_accelX = nomalizeX *mag;
+			_accelY = nomalizeY *mag;
+
+			float diff = Math::Sqrt(Math::Pow(plusRadius, 2) - sqrDistance);
+			_x += nomalizeX * diff * 0.5f;
+			_y += nomalizeY * diff * 0.5f;
+		}
+	}
+
+	void SetRadius(float radius)
+	{
+		_radius = radius;
+	}
+
+	float GetRadius()
+	{
+		return _radius;
+	}
+
+	void SetName(String name)
+	{
+		_name = name;
+	}
+
+	String GetName()
+	{
+		return _name;
+	}
+
+	void SetColor(ColorF color)
+	{
+		_color = color;
+	}
+
+	ColorF GetColor()
+	{
+		return _color;
+	}
+
+private:
+	const float CONST_GRAVITY_SCALE = 9.8f * 300;
+
+	float _radius = 10.0f;
+	float _x = 0.0f;
+	float _y = 0.0f;
+
+	ColorF _color = ColorF{ 0.5,0.5,0.5 };
+
+	String _name = U"";
+
+	float _accelX = 0.0f;
+	float _accelY = 0.0f;
+};
+
+
+void Main()
+{
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+
+	// 太文字のフォントを作成する | Create a bold font with MSDF method
+	const Font font{ FontMethod::MSDF, 24, Typeface::Bold };
+
+	const int FRUITS_MAX_NUM = 1024;
+
+	Fruit* fruits[FRUITS_MAX_NUM];
+
+	int cntFruit = 0;
+
+	E_Fruit_Type nextType = (E_Fruit_Type)Random((int)Sakuranbo, (int)MAX-1);
+
+	float mousePosX = 300.0f;
+	float mousePosY = 300.0f;
+
+	while (System::Update())
+	{
+
+		if (MouseL.down())
+		{
+			if (cntFruit >= FRUITS_MAX_NUM)
+			{
+				Print << U"Error!! Full Array Index";
+			}
+			else
+			{
+
+				Fruit* fruit = new Fruit(mousePosX, 100, nextType);
+				fruit->Type = nextType;
+				fruits[cntFruit++] = fruit;
+
+				nextType = (E_Fruit_Type)Random((int)Sakuranbo, (int)MAX-1);
+
+				Print << U"Pop!";
+			}
+		}
+
+		for (int i = 0; i < cntFruit; i++)
+		{
+			for (int j = 0; j < cntFruit; j++)
+			{
+				if (i != j)
+				{
+					// 当たり判定など
+					fruits[i]->SearchAndFusionAndDestroy(*fruits[j]);
+				}
+			}
+
+			fruits[i]->Logic(Scene::DeltaTime());
+			fruits[i]->Draw();
+		}
+
+		for (int i = 0; i < cntFruit; i++)
+		{
+			if (fruits[i]->IsDead)
+			{
+				for (int j = i; j < cntFruit - 1; j++)
+				{
+					// delete &fruits[j];
+					fruits[j] = fruits[j + 1];
+				}
+				cntFruit--;
+			}
+		}
+
+		mousePosX = Cursor::Pos().x;
+		mousePosY = Cursor::Pos().y;
+
+		if (mousePosX > 590) mousePosX = 590;
+		if (mousePosX < 210) mousePosX = 210;
+
+		// マウスカーソル部分に〇を表示
+		Circle(mousePosX, 100, 12).draw(Palette::Black);
+		Circle(mousePosX, 100, 10).draw(Palette::White);
+
+		Line(200, 500, 600, 500).draw(4, Palette::Orange);
+		Line(200, 150, 200, 500).draw(4, Palette::Orange);
+		Line(600, 150, 600, 500).draw(4, Palette::Orange);
+	}
+}
---
+++ 20251126_watermeron_game/Main.cpp
@@ -0,0 +1,156 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.15
+
+enum EElementType
+{
+	white,
+	red,
+	mazenta,
+	perple,
+	orange,
+	blue,
+	pink,
+	yellow,
+	cian,
+	green,
+	end_element
+};
+
+class Element
+{
+public:
+	EElementType elementType = white;
+
+	Element()
+	{
+		int elementMax = (int)EElementType::end_element;
+		int elementRandom = Random(0, elementMax-1);
+
+		SetUpElement((EElementType)elementRandom);
+	}
+
+	void SetUpElement(EElementType type)
+	{
+		int elementNum = (int)type;
+
+		_radius = (elementNum + 1) * 10.0f;
+
+		_color = GetColorByEElementType(type);
+	}
+
+	void Logic()
+	{
+
+
+		// 重力
+		_y++;
+	}
+
+	void Draw()
+	{
+		Circle(_x, _y, _radius).draw(_color);
+	}
+
+
+private:
+
+	float _x = 800/2;
+	float _y = 20;
+	float _radius = 1.0f;
+	ColorF _color = ColorF{ 1,1,1 };
+
+	bool isTatchWall = false;
+
+	ColorF GetColorByEElementType(EElementType type)
+	{
+		if (type == end_element)return ColorF{ 0,0,0 };
+
+		switch (type)
+		{
+		case EElementType::white:
+			return _color = ColorF{ 1,1,1 };
+		case EElementType::red:
+			return _color = ColorF{ 1,0,0};
+		case EElementType::mazenta:
+			return _color = ColorF{ 1,1,0 };
+		case EElementType::perple:
+			return _color = ColorF{ 0.7,0.5,0 };
+		case EElementType::orange:
+			return _color = ColorF{ 1,0,0.5 };
+		case EElementType::blue:
+			return _color = ColorF{ 0,0,1 };
+		case EElementType::pink:
+			return _color = ColorF{ 0.2,1,0 };
+		case EElementType::yellow:
+			return _color = ColorF{ 1,0,1 };
+		case EElementType::cian:
+			return _color = ColorF{ 0,0.2,1 };
+		case EElementType::green:
+			return _color = ColorF{ 0,1,0 };
+		}
+
+		return ColorF{ 0,0,0 };
+	}
+};
+
+class Wall
+{
+public:
+	Wall(float x, float y, float sizeX, float sizeY)
+	{
+		_x = x;
+		_y = y;
+		_sizeX = sizeX;
+		_sizeY = sizeY;
+	}
+
+	void Logic()
+	{
+
+	}
+
+	void Draw()
+	{
+		Rect(_x, _y, _sizeX, _sizeY).draw(ColorF{1,1,1});
+	}
+
+private:
+	float _x = 0.0f;
+	float _y = 0.0f;
+	float _sizeX = 200.0f;
+	float _sizeY = 200.0f;
+};
+
+void Main()
+{
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0, 0, 0 });
+
+	// 太文字のフォントを作成する | Create a bold font with MSDF method
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+
+	Element e = Element();
+
+	Wall buttomWall = Wall(100, 500, 600, 20);
+	Wall leftWall = Wall(100, 100, 20, 400);
+	Wall rightWall = Wall(680, 100, 20, 400);
+
+	while (System::Update())
+	{
+		buttomWall.Draw();
+		leftWall.Draw();
+		rightWall.Draw();
+
+		e.Logic();
+		e.Draw();
+	}
+}
+
+//
+// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
+//
+// - Release ビルド: 最大限の最適化でビルドします。
+//
+// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
+//
+// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
+//
