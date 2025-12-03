2028c7cd0cc4ce167ab262cb1216641b68c5d1ca
junya005 <junya005.65@gmail.com>
2025-12-03 15:40:17 +0900

    Update Main.cpp

--- 20251126_suica_sample/Main.cpp
+++ 20251126_suica_sample/Main.cpp
@@ -8,6 +8,7 @@ enum E_Fruit_Type
 	Dekopon,
 	Kaki,
 	Ringo,
+	Nashi,
 	Momo,
 	Pin,
 	Meron,
@@ -15,10 +16,13 @@ enum E_Fruit_Type
 	MAX
 };
 
-
 class Fruit
 {
 public:
+
+	bool _coledLeft = false;
+	bool _coledRight = false;
+	bool _coledButtom = false;
 
 	bool IsDead = false;
 
@@ -33,34 +37,42 @@ public:
 		_x = x;
 		_y = y;
 
-		float cross = (int)type * 1.3f;
+		SwitchType(type);
+	}
+
+	void SwitchType(E_Fruit_Type type)
+	{
+		float cross = Math::Pow(1.3f, (int)type);
 		if (cross != 0.0f) {
-			_radius = 5.0f *cross;
+			_radius = 10.0f * cross;
 		}
 		else
 		{
-			_radius = 5.0f;
+			_radius = 10.0f;
 		}
 
 		switch (type)
 		{
 		case Sakuranbo:
-			_color = ColorF( 0.9f,0.2f,0.1f );
+			_color = ColorF(0.9f, 0.2f, 0.1f);
 			break;
 		case Ichigo:
-			_color = ColorF(0.5f, 0.15f, 0.15f);
+			_color = ColorF(0.95f, 0.1f, 0.05f);
 			break;
 		case Budou:
-			_color = ColorF(0.25f, 0.6f, 0.095f);
-			break;
-		case Dekopon:
 			_color = ColorF(0.65f, 0.1f, 0.65f);
 			break;
+		case Dekopon:
+			_color = ColorF(0.85f, 0.8f, 0.0f);
+			break;
 		case Kaki:
-			_color = ColorF(0.6f, 0.1f, 0.05f);
+			_color = ColorF(0.95f, 0.4f, 0.05f);
 			break;
 		case Ringo:
-			_color = ColorF(0.9f, 0.05f, 0.05f);
+			_color = ColorF(0.91f, 0.25f, 0.25f);
+			break;
+		case Nashi:
+			_color = ColorF(0.75f, 0.75f, 0.05f);
 			break;
 		case Momo:
 			_color = ColorF(0.95f, 0.75f, 0.75f);
@@ -69,10 +81,10 @@ public:
 			_color = ColorF(0.95f, 0.7f, 0.15f);
 			break;
 		case Meron:
-			_color = ColorF(0.95f, 0.7f, 0.15f);
+			_color = ColorF(0.25f, 0.95f, 0.35f);
 			break;
 		case Suica:
-			_color = ColorF(0.15f, 0.45f, 0.125f);
+			_color = ColorF(0.05f, 0.45f, 0.125f);
 			break;
 		case MAX:
 			break;
@@ -81,46 +93,85 @@ public:
 		}
 	}
 
-	void Logic(float deltaTime)
+	void Logic(float deltaTime, int& gameOverCount)
 	{
+		_coledWithWall = false;
+		_coledRight = false;
+		_coledLeft = false;
+		_coledButtom = false;
+
 		if (IsDead) return;
 
-		// 加速度反映 動作反映
 		_x += _accelX * deltaTime;
 		_y += _accelY * deltaTime;
 
-		// 壁当たり
-		if(_x > 600 - _radius)
-		{
-			_x = 600 - _radius;
+		//　壁当たり
+		if (_x > 600 - _radius && 150 < _y) {
 
-			if (_accelX > 0) _accelX *= -0.25f;
+			if(_x >= 600)
+			{
+				_x = 600 + _radius;
+
+				if (_accelX < 0) _accelX *= -0.025f;
+				_coledLeft = true;
+			}
+			else
+			{
+				_x = 600 - _radius;
+
+				if (_accelX > 0) _accelX *= -0.025f;
+				_coledRight = true;
+			}
+
 		}
 
-		if (_x < 200 - _radius)
-		{
-			_x = 200 - _radius;
+		if (_x < 200 + _radius && 150 < _y) {
 
-			if (_accelX > 0) _accelX *= -0.25f;
+			if(_x <= 200)
+			{
+				_x = 200 - _radius;
+
+				if (_accelX > 0) _accelX *= -0.025f;
+				_coledRight = true;
+			}
+			else
+			{
+				_x = 200 + _radius;
+
+				if (_accelX < 0) _accelX *= -0.025f;
+				_coledLeft = true;
+			}
 		}
 
-		if(_y > 500)
-		{
+		if (_y > 500 - _radius && _x > 200 && _x < 600) {
 			_y = 500 - _radius;
 
-			if (_accelY > 0) _accelY *= -0.25f;
+			if (_accelY > 0) _accelY *= -0.025f;
+			_coledButtom = true;
+
 		}
 
-		// 振動禁止
-		if (_accelX >= -0.01f && _accelX <= 0.01f)_accelX = 0.0f;
-		if (_accelY >= -0.01f && _accelY <= 0.01f)_accelY = 0.0f;
+		_coledWithWall = _coledLeft || _coledRight || _coledButtom;
 
-		// 重力
+		// 振動禁止
+		if (_accelX >= -0.01f && _accelX <= 0.01f) _accelX = 0.0f;
+		if (_accelY >= -0.01f && _accelY <= 0.01f) _accelY = 0.0f;
+
+		// 加速度反映　動作反映
 		_accelY += deltaTime * CONST_GRAVITY_SCALE;
 
-		if(_y >= 600.0f + _radius)
+		_accelX *= 0.975f;
+		_accelY *= 0.975f;
+
+		if (_coledLeft && _accelX < 0)_accelX *= 0.9f;
+		if (_coledRight && _accelX > 0)_accelX *= 0.9f;
+		if (_coledButtom && _accelY > 0)_accelY *= 0.9f;
+
+		// GameOverフラグ
+		if (_y >= 600 + _radius)
 		{
 			IsDead = true;
+			gameOverCount ++ ;
 		}
 	}
 
@@ -131,54 +182,94 @@ public:
 		Circle{ _x, _y, _radius }.draw(_color);
 	}
 
-	void SearchAndFusionAndDestroy(Fruit& another)
+	void SearchAndFusionAndDestroy(Fruit* another)
 	{
-
+		if (IsDead)return;
 		if (_radius < 0) return;
-		if (another._radius < 0) another.IsDead = true;
 
-		Fruit* p1 = this;
-		Fruit* p2 = &another;
-
-		if (p1 == p2)
-		{
-			return;
-		}
-
-		float distX = another._x - _x;
-		float distY = another._y - _y;
+		float distX = another->_x - _x;
+		float distY = another->_y - _y;
 		float sqrDistance = distX * distX + distY * distY;
-		float plusRadius = another._radius + _radius;
+		float plusRadius = another->_radius + _radius;
 
 		// 二つの文明の活動領域がかぶった
 		if (plusRadius * plusRadius >= sqrDistance)
 		{
+			if(Type == another->Type)
+			{
+				if(Type == Suica)
+				{
+					IsDead = true;
+					another->IsDead = true;
+				}
+				else
+				{
+					Type = (E_Fruit_Type)((int)Type + 1);
+					another->IsDead = true;
+					_x += distX * 0.5f;
+					_y += distY * 0.5f;
+
+					SwitchType(Type);
+				}
+			}
+
+			float accelTheta = Math::Atan2(distY, distX);
+
+			// 壁に固定されていない場合は、めり込まないようにする
+			if (_coledLeft == false || _coledButtom == false || _coledRight == false)
+			{
+				float diff = Math::Sqrt(Math::Pow(plusRadius, 2) - sqrDistance);
+
+			
+				float shiftX = Math::Cos(accelTheta) * -diff * 0.5f;
+				float shiftY = Math::Sin(accelTheta) * -diff * 0.5f;
+
+				if (_coledLeft && shiftX < 0) shiftX = 0.0f;
+				if (_coledLeft && shiftX > 0) shiftX = 0.0f;
+				if (_coledButtom && shiftY > 0) shiftY = 0.0f;
+
+				_x += shiftX;
+				_y += shiftY;
+			}
+
 			float colTheta = Math::Atan2(distY,distX) + Math::Pi * 0.5f;
 			// colTheta + 360 = this + ref
-			float refTheta = colTheta + Math::Pi * 2.0f - Math::Atan2(_accelY, _accelX);
+			float refTheta = colTheta * 2.0f - Math::Atan2(_accelY, _accelX);
 
-			float mag = Math::Sqrt(Math::Pow(_accelY, 2) + Math::Pow(_accelX, 2));
+			float mag = (GetPotent() + another->GetPotent())*0.5f/_weight;
 
 			float nomalizeX = Math::Cos(refTheta);
 			float nomalizeY = Math::Sin(refTheta);
 
-			_accelX = nomalizeX *mag;
-			_accelY = nomalizeY *mag;
+			if ((_coledLeft || another->_coledLeft) && nomalizeX < 0.0f)nomalizeX = 0.0f;
+			if ((_coledRight || another->_coledRight) && nomalizeX > 0.0f)nomalizeX = 0.0f;
+			if ((_coledButtom || another->_coledButtom) && nomalizeY > 0.0f)nomalizeY = 0.0f;
 
-			float diff = Math::Sqrt(Math::Pow(plusRadius, 2) - sqrDistance);
-			_x += nomalizeX * diff * 0.5f;
-			_y += nomalizeY * diff * 0.5f;
+			_accelX = nomalizeX * mag * 0.25f;
+			_accelY = nomalizeY * mag * 0.25f;
 		}
+	}
+
+	float GetPotent()
+	{
+		if (_coledWithWall) return 9999999.0f;
+		return _weight * Math::Sqrt(Math::Pow(_accelY, 2) + Math::Pow(_accelX, 2));
 	}
 
 	void SetRadius(float radius)
 	{
 		_radius = radius;
+		_weight = 4.0f / 3.0f * Math::Pi * Math::Pow(_radius, 3);
 	}
 
 	float GetRadius()
 	{
 		return _radius;
+	}
+
+	float GetWeight()
+	{
+		return _weight;
 	}
 
 	void SetName(String name)
@@ -205,6 +296,7 @@ private:
 	const float CONST_GRAVITY_SCALE = 9.8f * 300;
 
 	float _radius = 10.0f;
+	float _weight = 4.0f / 3.0f * Math::Pi * Math::Pow(_radius,3);
 	float _x = 0.0f;
 	float _y = 0.0f;
 
@@ -214,8 +306,14 @@ private:
 
 	float _accelX = 0.0f;
 	float _accelY = 0.0f;
+
+	bool _coledWithWall = _coledLeft||_coledRight||_coledButtom;
 };
 
+bool GetGameOver(int& gameOverCount)
+{
+	return gameOverCount > 0;
+}
 
 void Main()
 {
@@ -223,7 +321,7 @@ void Main()
 	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 
 	// 太文字のフォントを作成する | Create a bold font with MSDF method
-	const Font font{ FontMethod::MSDF, 24, Typeface::Bold };
+	const Font font{ FontMethod::MSDF, 24, Typeface::Bold};
 
 	const int FRUITS_MAX_NUM = 1024;
 
@@ -236,10 +334,19 @@ void Main()
 	float mousePosX = 300.0f;
 	float mousePosY = 300.0f;
 
+	int gameOverCount = 0;
+
+	bool RetryButtonDown = false;
+
+	const ColorF buttonOnColor = ColorF(0.1);
+	const ColorF buttonOffColor = ColorF(0.9);
+
 	while (System::Update())
 	{
+		bool gameOver = GetGameOver(gameOverCount);
 
-		if (MouseL.down())
+
+		if (MouseL.down() && gameOver == false)
 		{
 			if (cntFruit >= FRUITS_MAX_NUM)
 			{
@@ -254,50 +361,103 @@ void Main()
 
 				nextType = (E_Fruit_Type)Random((int)Sakuranbo, (int)MAX-1);
 
-				Print << U"Pop!";
+				// Print << U"Pop!";
 			}
 		}
 
 		for (int i = 0; i < cntFruit; i++)
 		{
-			for (int j = 0; j < cntFruit; j++)
+			if(gameOver==false)
 			{
-				if (i != j)
+				for (int j = 0; j < cntFruit; j++)
 				{
-					// 当たり判定など
-					fruits[i]->SearchAndFusionAndDestroy(*fruits[j]);
+					if (i != j)
+					{
+						// 当たり判定など
+						fruits[i]->SearchAndFusionAndDestroy(fruits[j]);
+					}
 				}
-			}
 
-			fruits[i]->Logic(Scene::DeltaTime());
+				fruits[i]->Logic(Scene::DeltaTime(), gameOverCount);
+			}
+			
 			fruits[i]->Draw();
 		}
 
-		for (int i = 0; i < cntFruit; i++)
+		if (gameOver == false)
 		{
-			if (fruits[i]->IsDead)
+			for (int i = 0; i < cntFruit; i++)
 			{
-				for (int j = i; j < cntFruit - 1; j++)
+				if (fruits[i]->IsDead)
 				{
-					// delete &fruits[j];
-					fruits[j] = fruits[j + 1];
+					for (int j = i; j < cntFruit - 1; j++)
+					{
+						// delete &fruits[j];
+						fruits[j] = fruits[j + 1];
+					}
+					cntFruit--;
 				}
-				cntFruit--;
 			}
+
+			
+
+			if (mousePosX > 590) mousePosX = 590;
+			if (mousePosX < 210) mousePosX = 210;
+
+			// マウスカーソル部分に〇を表示
+			Circle(mousePosX, 100, 12).draw(Palette::Black);
+			Circle(mousePosX, 100, 10).draw(Palette::White);
 		}
 
 		mousePosX = Cursor::Pos().x;
 		mousePosY = Cursor::Pos().y;
 
-		if (mousePosX > 590) mousePosX = 590;
-		if (mousePosX < 210) mousePosX = 210;
-
-		// マウスカーソル部分に〇を表示
-		Circle(mousePosX, 100, 12).draw(Palette::Black);
-		Circle(mousePosX, 100, 10).draw(Palette::White);
-
 		Line(200, 500, 600, 500).draw(4, Palette::Orange);
 		Line(200, 150, 200, 500).draw(4, Palette::Orange);
 		Line(600, 150, 600, 500).draw(4, Palette::Orange);
+
+		if(gameOver)
+		{
+			Rect rect = Rect{ 0, 0, 800, 600 }.draw(ColorF{ 0,0,0,0.85f });
+
+			font(U"GAME OVER").draw(60, rect.center()+Vec2(-40*9*0.5f, -60), ColorF(0.8f));
+
+			ColorF aC;
+			ColorF bC;
+
+			if(RetryButtonDown)
+			{
+				aC = buttonOnColor;
+				bC = buttonOffColor;
+			}
+			else
+			{
+				aC = buttonOffColor;
+				bC = buttonOnColor;
+			}
+
+			Rect rect_button = Rect{ 360, 370, 120, 60 }.draw(buttonOffColor);
+
+			font(U"Retry").draw(30, rect_button.center() + Vec2(-20 * 5 * 0.5f, -30), buttonOnColor);
+
+			if ((mousePosX > 360 && mousePosX < 480 && mousePosY > 370 && mousePosY < 430)==false)
+			{
+				RetryButtonDown = true;
+			}
+
+			if (MouseL.down())
+			{
+				if(mousePosX>360 && mousePosX < 480 && mousePosY > 370 && mousePosY < 430)
+				{
+					RetryButtonDown = true;
+				}
+			}
+
+			if(MouseL.up()&& RetryButtonDown)
+			{
+				// 一旦ゲームオーバーを解く
+				gameOverCount = 0;
+			}
+		}
 	}
 }
